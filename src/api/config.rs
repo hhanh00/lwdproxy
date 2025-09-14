5bc9d68b39051202fa8724560045a85c031e92bd
Hanh <hanh425@gmail.com>
2025-09-15 07:20:21 +0800

    fix: do not exit sync loop on error

--- src/api/config.rs
+++ src/api/config.rs
@@ -1,8 +1,6 @@
-use flutter_rust_bridge::frb;
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize, Debug, Default)]
-#[frb(dart_metadata=("freezed"))]
 pub struct Config {
     pub db_path: String,
     pub origin: Vec<String>,
